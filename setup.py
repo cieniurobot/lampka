5a3776aa33f28954b658ead36a89eabc4e9ccf66
mplociennik <mplociennik@trans.eu>
2018-01-24 13:49:10 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lampka',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/cieniurobot/lampka',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
